41ae93435543926803fbdb4a4756d5f42e8aeeb2
kliuchyk <kliuchyk@gmail.com>
2020-11-22 16:46:57 +0200

    setup basic backend with graphql api

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+
+import { schema } from "./graphql";
+
+const app = express();
+const PORT = 9000;
+
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: "/api" });
+
+app.listen(PORT);
+
+console.log(`[app]: http://localhost:${PORT}`);
