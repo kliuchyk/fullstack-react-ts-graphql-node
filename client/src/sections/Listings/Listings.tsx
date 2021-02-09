bf53db2fe79de6e5b452c32bbd67972883633e16
kliuchyk <kliuchyk@gmail.com>
2021-02-09 11:19:19 +0200

    add listings component with graphql interaction

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,56 @@
-import React from 'react';
+import React from "react";
+import { server } from "../../lib/api";
+import {
+  ListingsData,
+  DeleteListingData,
+  DeleteListingVariables,
+} from "./types";
 
-export const Listings = () => {
-  return <h2>Listings Header</h2>
+const LISTINGS = `
+  query Listings {
+    listings {
+      id,
+      address,
+      price,
+      rating,
+      title, 
+      numOfGuests,
+      numOfBeds,
+      numOfBaths,
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListings(id: $id) {
+      id,
+    }
+  }
+`;
+
+interface Props {
+  title: string;
 }
+
+export const Listings = (props: Props) => {
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    return data;
+  };
+
+  const deleteListings = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({ query: DELETE_LISTING, variables: { id: "5fe9dfcc9391bc97bec2d7a9" } });
+  };
+
+  return (
+    <div>
+      <h2>Listings Header</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={deleteListings}>Delete a listing!</button>
+    </div>
+  );
+};
