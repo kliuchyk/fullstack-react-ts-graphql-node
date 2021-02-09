d7ce50f214d7a49416359e3d0f7279d2dd37b880
kliuchyk <kliuchyk@gmail.com>
2021-02-09 16:14:00 +0200

    move to useReducer from useState logic

--- client/src/lib/api/useMutation.ts
+++ client/src/lib/api/useMutation.ts
@@ -1,5 +1,5 @@
 import { server } from "./server";
-import { useCallback, useState, useMemo } from "react";
+import { useCallback, useMemo, useReducer } from "react";
 
 interface State<TData> {
   data: TData | null;
@@ -12,10 +12,32 @@ type MutationTuple<TData, TVariables> = [
   State<TData>
 ];
 
+type Action<TData> =
+  | { type: "FETCH" }
+  | { type: "FETCH_SUCCESS"; payload: TData }
+  | { type: "FETCH_ERROR" };
+
+const reducer = <TData>() => (
+  state: State<TData>,
+  action: Action<TData>
+): State<TData> => {
+  switch (action.type) {
+    case "FETCH":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { data: action.payload, loading: false, error: false };
+    case "FETCH_ERROR":
+      return { ...state, error: true, loading: false };
+    default:
+      throw new Error();
+  }
+};
+
 export const useMutation = <TData = any, TVariables = any>(
   query: string
 ): MutationTuple<TData, TVariables> => {
-  const [state, setState] = useState<State<TData>>({
+  const fetchReducer = reducer<TData>();
+  const [state, dispatch] = useReducer(fetchReducer, {
     data: null,
     loading: false,
     error: false,
@@ -24,7 +46,7 @@ export const useMutation = <TData = any, TVariables = any>(
   const fetch = useCallback(
     async (variables?: TVariables) => {
       try {
-        setState({ data: null, loading: true, error: false });
+        dispatch({ type: "FETCH" });
 
         const { data, errors } = await server.fetch<TData, TVariables>({
           query,
@@ -35,9 +57,9 @@ export const useMutation = <TData = any, TVariables = any>(
           throw new Error(errors[0].message);
         }
 
-        setState({ data, loading: false, error: false });
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
       } catch (err) {
-        setState({ data: null, loading: false, error: true });
+        dispatch({ type: "FETCH_ERROR" });
         throw console.error(err);
       }
     },
